91d25d29de5d3a830b3cabd38646bb6904c1b089
NickDil <nick.dillen@ugent.be>
2018-08-30 20:04:29 +0200

    moved to https://github.ugent.be/ndillen/Master-Thesis-MMETSP.git

--- Delete/del.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-module load python/x86_64/3.5.1
-python3 /home/nidil/Documents/Project_TRAPID/Scripts/Delete/delete_experiments.py 9939 10022 /group/transreg/nidil/bigupload/experiment_data/ /home/nidil/Documents/Project_TRAPID/Scripts/config_connect_trapid.json
--- flame-clustering/cluster_FLAME_cluster.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#specify axis, knn and d with -v MY_AXIS=0,MY_KNN=15,MY_D=4
-module load python/x86_64/3.5.1
-python3 /home/nidil/Documents/Project_TRAPID/Scripts/Compare/python/python_scripts/FLAME_clustering.py /home/nidil/Documents/Project_TRAPID/Scripts/Compare/Results/REASSEMBLIES_V2_results/NOG_vectors_mincomp0.6_minocc2_None_counts/NOG_vectors_mincomp0.6_minocc2_None_counts.txt /home/nidil/Documents/Project_TRAPID/Scripts/flame-clustering/sample -axis $MY_AXIS -d $MY_D -knn $MY_KNN -write
